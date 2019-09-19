399530f97c8e19a6120b28801c40d5e5ffb3fbae
Mateusz Panek <mateusz.m.panek@gmail.com>
2019-09-19 21:07:36 +0200

    Codewars Kata - The Lamp: Revisited

---
+++ CodeWars/the_lamp_revisited.py
@@ -0,0 +1,20 @@
+"""
+Define a class called Lamp. It will have a string attribute for color and boolean attribute, on, that will refer to whether the lamp is on or not. Define your class constructor with a parameter for color and assign on as false on initialize.
+
+Give the lamp an instance method called toggle_switch that will switch the value of the on attribute.
+
+Define another instance method called state that will return "The lamp is on." if it's on and "The lamp is off." otherwise.
+"""
+
+
+class Lamp(object):
+
+    def __init__(self, color: str, on: bool = False):
+        self.color = color
+        self.on = on
+
+    def toggle_switch(self):
+        self.on = not self.on
+
+    def state(self):
+        return 'The lamp is {}.'.format('on' if self.on is True else 'off')
